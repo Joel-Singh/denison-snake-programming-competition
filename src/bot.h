66ce7d378b376f028055d0ac6a553fb61498f20b
Joel Singh <joelsingh788@gmail.com>
2025-07-24 00:07:01 -0400

    Write docs for `Bot`

--- src/bot.h
+++ src/bot.h
@@ -3,8 +3,13 @@
 #include "grid.h"
 #include "pos.h"
 
+/// \brief The class that every snake bot derives from. If you don't understand
+/// what that means don't worry! Just focus implementing the logic for your bot
+/// in `MyBot.cpp`.
 class Bot {
 public:
   Bot() {}
+  /// \brief The function that determines how a bot moves by looking at the
+  /// @ref Grid
   virtual Direction think(const Grid &grid) = 0;
 };
