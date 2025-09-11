d6d7dd477d866a7537e51eb156d34cb63e2530bd
Joel Singh <joelsingh788@gmail.com>
2025-09-10 23:36:00 -0400

    Move GRID_SIZE into game_settings.h

--- src/game_settings.h
+++ src/game_settings.h
@@ -2,3 +2,5 @@
 /// draw) at the end of this tick. Grid::get_current_tick will only ever be as
 /// high as FINAL_TICK
 const unsigned int FINAL_TICK = 100;
+
+const int GRID_SIZE = 20; // Square grid
