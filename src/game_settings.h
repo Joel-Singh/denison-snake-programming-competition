e0d92bd89d42410b796168ae89338f42c49fef74
Joel Singh <joelsingh788@gmail.com>
2025-10-11 11:14:52 -0400

    Increase FINAL_TICK and rename CELL_SIZE

--- src/game_settings.h
+++ src/game_settings.h
@@ -3,11 +3,11 @@
 /// \brief The final tick that the game runs on. Will determine a winner (or
 /// draw) at the end of this tick. Grid::get_current_tick will only ever be as
 /// high as FINAL_TICK
-const unsigned int FINAL_TICK = 100;
+const unsigned int FINAL_TICK = 300;
 
 /// \brief the size of the square grid the game takes place upon. Also
 /// obtainable with Grid::ge_height and Grid::get_width
-const int CELL_SIZE = 20; // Square grid
+const int GRID_SIZE = 20; // Square grid
 
 /// \brief the limit of ram in megabytes bots in the tournament are run at.
 const int RAM_LIMIT = 1000;
