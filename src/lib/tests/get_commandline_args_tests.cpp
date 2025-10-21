1e035d11f34228359e3881a26354541fd01f954a
Joel Singh <joelsingh788@gmail.com>
2025-10-21 16:24:13 -0400

    Add more tests to make sure multiple player types throws

--- src/lib/tests/get_commandline_args_tests.cpp
+++ src/lib/tests/get_commandline_args_tests.cpp
@@ -1,10 +1,31 @@
 #include "lib/get_commandline_args.h"
 #include <gtest/gtest.h>
 
-TEST(get_commandline_args, throws_on_too_many_args) {
-  char *argv[] = {(char *)"adsf", (char *)"--evil-bot", (char *)"--yourself"};
+TEST(get_commandline_args, throws_on_specifying_multiple_player_types) {
+  char *argv1[] = {(char *)"./main", (char *)"--evil-bot",
+                   (char *)"--yourself"};
+  EXPECT_THROW(get_commandline_args(3, argv1), std::invalid_argument);
 
-  EXPECT_THROW(get_commandline_args(3, argv), std::invalid_argument);
+  char *argv2[] = {(char *)"./main", (char *)"--evil-bot", (char *)"--my-bot"};
+  EXPECT_THROW(get_commandline_args(3, argv2), std::invalid_argument);
+
+  char *argv3[] = {(char *)"./main", (char *)"--yourself",
+                   (char *)"--evil_bot"};
+  EXPECT_THROW(get_commandline_args(3, argv3), std::invalid_argument);
+
+  char *argv4[] = {(char *)"./main", (char *)"--yourself", (char *)"--my-bot"};
+  EXPECT_THROW(get_commandline_args(3, argv4), std::invalid_argument);
+
+  char *argv5[] = {(char *)"./main", (char *)"--my-bot", (char *)"--evil-bot"};
+  EXPECT_THROW(get_commandline_args(3, argv5), std::invalid_argument);
+
+  char *argv6[] = {(char *)"./main", (char *)"--my-bot", (char *)"--evil-bot",
+                   (char *)"--yourself"};
+  EXPECT_THROW(get_commandline_args(4, argv6), std::invalid_argument);
+
+  char *argv7[] = {(char *)"./main", (char *)"--evil-bot", (char *)"--my-bot",
+                   (char *)"--yourself"};
+  EXPECT_THROW(get_commandline_args(4, argv7), std::invalid_argument);
 }
 
 TEST(get_commandline_args, throws_on_incorrect_option) {
